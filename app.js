7a1b5a8616757cf5cb44abfcd1782a659d0bbfc6
XxrrxX <xxrrxx3888@gmail.com>
2024-06-01 17:19:01 -0600

    update

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const argv = require('yargs').argv;
 console.log("Iniciado");
 const http = require('http');
 http.createServer((req,res )=>{
-    console.log(req);
     res.write('hola mundo');
     res.end();
-}).listen(5555);
-console.log('escuchando en el puerto',5555);
+}).listen(8080);
+console.log('escuchando en el puerto',8080);
