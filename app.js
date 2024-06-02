5e2631b27683f3e4b40bab675202c3a6c6005088
XxrrxX <79820165+XxrrxX@users.noreply.github.com>
2024-06-02 16:03:18 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express')
+const hbs = require('hbs');
+require('dotenv').config();
 const app = express()
-const port = 3131;
-//require('hbs');
+const port = process.env.PORT;
+//handlebars
 app.set('view engine','hbs');
+hbs.registerPartials(__dirname + '/views/partials', function (err) {});
 //servir contenido estatico
 app.use(express.static('public'));
 
-app.get('/', function (req, res) {
+app.get('/home', function (req, res) {
   res.render('home');
 })
 
@@ -16,7 +19,7 @@ app.get('/login', function (req, res) {
   })
 
 app.get('/registro', function (req, res) {
-  res.sendFile(__dirname+'/public/registro.html');
+    res.render('registro');
 })
 
 app.get('*', function (req, res) {
