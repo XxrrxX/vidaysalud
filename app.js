b8b59cb752d8b8416cda7a36397ff5c89e6d6fa4
XxrrxX <xxrrxx3888@gmail.com>
2024-06-02 00:35:46 -0600

    update

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
-const argv = require('yargs').argv;
-console.log("Iniciado");
-const http = require('http');
-http.createServer((req,res )=>{
-    res.write('hola mundo');
-    res.end();
-}).listen(8080);
-console.log('escuchando en el puerto',8080);
+const express = require('express')
+const app = express()
+const port = 3131;
+//require('hbs');
+app.set('view engine','hbs');
+//servir contenido estatico
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+  res.render('home');
+})
+
+
+app.get('/login', function (req, res) {
+    res.send('Bienvenido a login');
+  })
+
+app.get('/registro', function (req, res) {
+  res.sendFile(__dirname+'/public/registro.html');
+})
+
+app.get('*', function (req, res) {
+  res.send('404 | Page not found');
+})
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  })
