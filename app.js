b1d21b204db83ae005a9a05313f0e53a089bb1d8
XxrrxX <79820165+XxrrxX@users.noreply.github.com>
2024-06-02 19:40:57 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,32 +1,6 @@
-const express = require('express')
-const hbs = require('hbs');
 require('dotenv').config();
-const app = express()
-const port = process.env.PORT;
-//handlebars
-app.set('view engine','hbs');
+const hbs = require('hbs');
+const Server = require('./models/server');
+const server = new Server();
 hbs.registerPartials(__dirname + '/views/partials', function (err) {});
-//servir contenido estatico
-app.use(express.static('public'));
-
-app.get('/home', function (req, res) {
-  res.render('home');
-})
-
-
-app.get('/login', function (req, res) {
-    res.send('Bienvenido a login');
-  })
-
-app.get('/registro', function (req, res) {
-    res.render('registro');
-})
-
-app.get('*', function (req, res) {
-  res.send('404 | Page not found');
-})
-
-
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
-  })
+server.listen();
