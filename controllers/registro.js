0b18682b9262af1553eb721ea7111d0b94a9c39f
XxrrxX <79820165+XxrrxX@users.noreply.github.com>
2024-06-04 02:36:47 -0600

    Update registro.js

--- controllers/registro.js
+++ controllers/registro.js
@@ -1,55 +1,23 @@
 const { response } = require('express');
 const fs = require('fs');
+const Usuario = require('../models/usuario');
 const registroGet = (req, res = response) => {
     res.render('registro');
 }
  
 
-const registroPost =  (req, res = response) => {
-    
-    fs.readFile('./DBjson/Usuarios.json', 'utf8', (err, data) => {
-        
-        if (err) {
-            console.error('Error al leer el archivo:', err);
-            return;
-        }
-        
-        if(data.length == 0){
-            console.log("Sin datos");
-            let datosjson = [{"usuario":req.query.usuario,"password":req.query.password}];
-            fs.appendFile('./DBjson/Usuarios.json', JSON.stringify(datosjson), function (err) {
-                if (err) {
-                    console.error('Falló la escritura:', err);
-                } else {
-                    console.log('Datos base creada correctamente en DBJson '+ JSON.stringify(datosjson)) + 'Estatus 201';
-                    res.status(201).send({ success: true });
-                }
-            });
-        }else{
-            const datosjson = JSON.parse(data);
-           
-            function exUsuario(Usuario) {
-                return Usuario.usuario === req.query.usuario;
-            }            
-            const UsuarioEncontrado = datosjson.find(exUsuario);
-            if (UsuarioEncontrado) {
-                console.log('Usuario existente estatus 409');
-                console.log(datosjson);
-                res.status(409).send({ success: false });
-            }else{
-            datosjson.push({"usuario":req.query.usuario,"password":req.query.password});
-            fs.writeFileSync('./DBjson/Usuarios.json', JSON.stringify(datosjson));
-            console.log('Nuevo usuario agregado estatus 201');
-            console.log(datosjson);
-            res.status(201).send({ success: true });
-            }
-        }
-        
-    });
+const registroPost = async (req, res = response) => {
+
+              //let datosjson = [{"usuario":req.query.usuario,"password":req.query.password}];
+
+              const body = req.body;
+              const usuario = new Usuario(body);
+              await usuario.save();    
+              res.json({msg:'usuario post',usuario});
+            
+    };
         
     
- // res.status(200).json({"usuario":req.query.usuario,"password":req.query.password});
-}
 
 
 module.exports = {
