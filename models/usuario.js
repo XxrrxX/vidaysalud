1d5164a59cf2301812ab5c576018371b53d4764d
XxrrxX <79820165+XxrrxX@users.noreply.github.com>
2024-06-04 01:40:18 -0600

    Update usuario.js

---
+++ models/usuario.js
@@ -0,0 +1,30 @@
+const {Schema, model} = require('mongoose');
+const { type } = require('os');
+const { string } = require('yargs');
+const UsusarioSchema = Schema({
+    nombre:{
+        type:String,
+        required:[true,'El nombre es obligatorio'],
+        unique:true
+    },
+    password:{
+        type:String,
+        required:[true,'El password es obligatorio']
+    },
+    rol:{
+        type:String,
+        required:[true,'El rol es obligatorio'],
+        enum:['ADMIN_ROLE','USER_ROLE']
+    },
+    estado:{
+        type:Boolean,
+        default:true
+    },
+    google:{
+        type:Boolean,
+        default:false
+    }
+});
+
+
+module.exports = model('Usuarios',UsusarioSchema);
