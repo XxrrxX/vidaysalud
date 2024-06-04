566f77522505281482445aae737d46ca29d6690d
XxrrxX <79820165+XxrrxX@users.noreply.github.com>
2024-06-04 02:36:51 -0600

    Update server.js

--- models/server.js
+++ models/server.js
@@ -23,8 +23,8 @@ async condb(){
 middlewares(){
     //cors
     this.app.use(cors());
-    this.app.use(express.urlencoded({extended:true}));
     this.app.use(express.json());
+    this.app.use(express.urlencoded({extended:true}));
     //directprio publico
     this.app.use(express.static('public'));
     this.app.set('view engine','hbs');
