cfcc1ae0942780e7ae16bdf58c8fa1c12acbfa79
XxrrxX <79820165+XxrrxX@users.noreply.github.com>
2024-06-03 20:04:38 -0600

    Update registro.js

--- controllers/registro.js
+++ controllers/registro.js
@@ -8,31 +8,45 @@ const registroGet = (req, res = response) => {
 const registroPost =  (req, res = response) => {
     
     fs.readFile('./DBjson/Usuarios.json', 'utf8', (err, data) => {
+        
         if (err) {
             console.error('Error al leer el archivo:', err);
             return;
         }
-        console.log('Nuevo usuario agregado: Estatus 201');
         
         if(data.length == 0){
             console.log("Sin datos");
-          let datosjson = [{"usuario":req.query.usuario,"password":req.query.password}];
+            let datosjson = [{"usuario":req.query.usuario,"password":req.query.password}];
             fs.appendFile('./DBjson/Usuarios.json', JSON.stringify(datosjson), function (err) {
                 if (err) {
                     console.error('Falló la escritura:', err);
                 } else {
-                    console.log('Datos escritos correctamente en DBJson '+ JSON.stringify(datosjson));
+                    console.log('Datos base creada correctamente en DBJson '+ JSON.stringify(datosjson)) + 'Estatus 201';
+                    res.status(201).send({ success: true });
                 }
             });
         }else{
-            let datosjson = JSON.parse(data);
+            const datosjson = JSON.parse(data);
+           
+            function exUsuario(Usuario) {
+                return Usuario.usuario === req.query.usuario;
+            }            
+            const UsuarioEncontrado = datosjson.find(exUsuario);
+            if (UsuarioEncontrado) {
+                console.log('Usuario existente estatus 409');
+                console.log(datosjson);
+                res.status(409).send({ success: false });
+            }else{
             datosjson.push({"usuario":req.query.usuario,"password":req.query.password});
             fs.writeFileSync('./DBjson/Usuarios.json', JSON.stringify(datosjson));
+            console.log('Nuevo usuario agregado estatus 201');
             console.log(datosjson);
+            res.status(201).send({ success: true });
+            }
         }
         
     });
-        res.status(201).render('registro');
+        
     
  // res.status(200).json({"usuario":req.query.usuario,"password":req.query.password});
 }
