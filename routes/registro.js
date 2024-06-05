e833c5d69ef94f8b6b0d2695b683a4555b8c7201
XxrrxX <79820165+XxrrxX@users.noreply.github.com>
2024-06-05 06:18:03 -0600

    Update registro.js

--- routes/registro.js
+++ routes/registro.js
@@ -11,6 +11,8 @@ router.get('/',registroGet);
 router.post('/',[ 
     check('nombre','nombre no valido').not().isEmpty(),
     check('correo','correo no valido').isEmail(),
+    check('password','password no valido').not().isEmpty(),
+    check('rol','rol no valido').not().isEmpty(),
 ],registroPost);
 
 module.exports = router;
