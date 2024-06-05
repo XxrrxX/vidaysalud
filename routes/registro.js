0799c4a517fc1a466893df0c3bdc95b621b1bf12
XxrrxX <79820165+XxrrxX@users.noreply.github.com>
2024-06-05 04:22:53 -0600

    Update registro.js

--- routes/registro.js
+++ routes/registro.js
@@ -2,11 +2,12 @@ const { Router } = require('express');
 const {
     registroGet, 
     registroPost } = require('../controllers/registro');
+const { check } = require('express-validator');
 
 const router = Router();
 
 
 router.get('/',registroGet);
-router.post('/',registroPost);
+router.post('/',[ check('correo','El correo no es valido').isEmail(),],registroPost);
 
 module.exports = router;
