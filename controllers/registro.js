d155dc3aa1ad74b9eec6370b708eed05c227fa27
XxrrxX <79820165+XxrrxX@users.noreply.github.com>
2024-06-04 22:15:15 -0600

    Update registro.js

--- controllers/registro.js
+++ controllers/registro.js
@@ -2,7 +2,9 @@ const { response } = require('express');
 const fs = require('fs');
 const Usuario = require('../models/usuario');
 const bcryptjs = require('bcryptjs');
+
 const registroGet = (req, res = response) => {
+    console.log('GET /Registro estatus 200')
     res.render('registro');
 }
  
@@ -14,9 +16,16 @@ const registroPost = async (req, res = response) => {
               const {nombre , correo, password , rol } = req.body;
               const usuario = new Usuario({nombre ,correo , password , rol });
                 //Verifivcar el correo
+                const Existemail = await Usuario.findOne({correo});
+                if(Existemail){
+                    console.log('POST /registro El correo ya esta registrado estatus 400');
+                    return res.status(400).json({
+                        msg:'Este correo ya esta registrado'
+                    })
+                }
                 //Encriptar Contra
                 const salt = bcryptjs.genSaltSync(10);
-                usuario.password = bcryptjs.hashSync(password, salt);
+                usuario.password = bcryptjs.hashSync(password, salt );
                 //Guardar en la DB
 
 
