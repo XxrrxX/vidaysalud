c63c85e36bb03500f14a1823ff168bf0763e1583
XxrrxX <79820165+XxrrxX@users.noreply.github.com>
2024-06-10 01:36:42 -0600

    Create validar-login.js

---
+++ public/assets/js/validar-login.js
@@ -0,0 +1,34 @@
+document.getElementById('frmreg').reset();
+
+let validar = (id,lbl,dato,df)=>{
+
+    if(document.getElementById(id).value.length == 0){
+        document.getElementById(lbl).style.color='red';        
+        document.getElementById(id).style.borderBlockColor='red';
+
+        if(df.length == 0){
+            df = dato;
+        }else{
+            df = `${df}, ${dato}`;
+        }        
+    }else{
+        document.getElementById(lbl).style.color='green';        
+        document.getElementById(id).style.borderBlockColor='green';
+    }
+    return df;
+}
+
+
+function enviar(){
+var datosF = "";
+datosF = validar('n','lblu','nombre',datosF);
+datosF = validar('c','lblc','correo',datosF);
+datosF = validar('p1','lblp1','password',datosF);
+
+if(datosF.length > 0){
+    alert(`Faltan los siguientes datos: \n ${datosF}`);
+}else{
+    
+}
+
+}
