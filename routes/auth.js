40787819e44ff41ec53f720ccd3bc438f7a2b10e
XxrrxX <79820165+XxrrxX@users.noreply.github.com>
2024-06-10 02:43:22 -0600

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,14 @@
 const { Router } = require('express');
-const {
-        authPost } = require('../controllers/auth');
+const {authPost } = require('../controllers/auth');
+const { check } = require('express-validator');
+const { ValidarCampos } = require('../middlewares/validar-campos');
+
 const router = Router();
 
-router.post('/',authPost);
+router.post('/',[
+        check('correo','El correo es obligatorio').isEmail(),
+        check('password','El password es obligatorio').not().isEmpty(),
+        ValidarCampos
+],authPost);
 
 module.exports = router;
