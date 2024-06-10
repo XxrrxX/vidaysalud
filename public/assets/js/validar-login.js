0c21776a5d22e3460a22db78049fac18555c6a24
XxrrxX <79820165+XxrrxX@users.noreply.github.com>
2024-06-10 03:29:39 -0600

    Update validar-login.js

--- public/assets/js/validar-login.js
+++ public/assets/js/validar-login.js
@@ -19,7 +19,7 @@ let validar = (id,lbl,dato,df)=>{
 }
 
 
-function enviar(){
+function login(){
 var datosF = "";
 datosF = validar('c','lblc','correo',datosF);
 datosF = validar('p1','lblp1','password',datosF);
@@ -27,6 +27,37 @@ datosF = validar('p1','lblp1','password',datosF);
 if(datosF.length > 0){
     alert(`Faltan los siguientes datos: \n ${datosF}`);
 }else{
+    let c = document.getElementById('c').value;
+    let p = document.getElementById('p1').value;
+    fetch('/auth', {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json' // Tipo de contenido
+        },
+        body: JSON.stringify({correo: c, password: p}) // Datos a enviar
+    }).then(response => {
+        if (response.ok) {
+            return response.text();
+        } else {
+            alert("A ocurrido un error inesperado!");
+            throw new Error('Error en la llamada Ajax');
+        }
+    })
+    .then(data => {
+        console.log('Respuesta del servidor:', data);
+        let datosjson = JSON.parse(data);
+        if(datosjson.msg == "sucess"){
+            alert('Inicio exitoso');
+            location.reload();            
+        }else{
+            alert(datosjson.msg);
+            location.reload();
+        }
+
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
     
 }
 
