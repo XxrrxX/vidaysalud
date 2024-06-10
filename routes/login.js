4d4254778db4e8676df21975d6d9e39e683e8061
XxrrxX <79820165+XxrrxX@users.noreply.github.com>
2024-06-10 02:19:04 -0600

    Update login.js

--- routes/login.js
+++ routes/login.js
@@ -1,10 +1,9 @@
 const { Router } = require('express');
 const {
     loginGet, 
-    loginPost } = require('../controllers/login');
+     } = require('../controllers/login');
 const router = Router();
 
 router.get('/',loginGet);
-router.post('/',loginPost);
 
 module.exports = router;
