3b21f4d9bdf2f283c9e1b11d7b88bcaac3e92f07
XxrrxX <79820165+XxrrxX@users.noreply.github.com>
2024-06-09 22:17:21 -0600

    Update registro.js

--- routes/registro.js
+++ routes/registro.js
@@ -3,6 +3,7 @@ const {
     registroGet, 
     registroPost } = require('../controllers/registro');
 const { check } = require('express-validator');
+const { ValidarCampos } = require('../middlewares/validar-campos');
 
 const router = Router();
 
@@ -13,6 +14,7 @@ router.post('/',[
     check('correo','correo no valido').isEmail(),
     check('password','password no valido requiere minimo 6 caracteres').isLength({min:6}),
     check('rol','rol no valido').isIn(['ADMIN_ROLE','USER_ROLE']),
+    ValidarCampos
 ],registroPost);
 
 module.exports = router;
