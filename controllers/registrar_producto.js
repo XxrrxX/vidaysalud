9265d3eb40db7174079a90492de1b3f456ad5a37
XxrrxX <79820165+XxrrxX@users.noreply.github.com>
2024-06-14 13:58:06 -0600

    Create registrar_producto.js

---
+++ controllers/registrar_producto.js
@@ -0,0 +1,34 @@
+const { response } = require('express');
+const bcryptjs = require('bcryptjs');
+const Producto = require('../models/productos');
+ 
+const registroPost = async (req, res = response) => {
+            
+  const {nombre_del_producto , cantidad , Precio_compra,Precio_venta } = req.body;
+  const producto = new Producto({nombre_del_producto, cantidad , Precio_compra,Precio_venta });
+  //Verifivcar el correo
+  const Existproducto = await Producto.findOne({nombre_del_producto});
+  if(Existproducto){
+    const stok = Existproducto.cantidad + cantidad;
+    const body = req.body;
+    body.cantidad = stok;
+    const { Precio_compra ,Precio_venta, ...resto} = body;
+    const  id  = Existproducto._id;
+    console.log(id);
+    const producto = await Producto.findByIdAndUpdate(id, resto);
+     console.log('POST /registro El producto ya esta registrado estatus 203');
+     return res.status(203).json({ msg:'producto actualizado'});
+  }else{
+  //Guardar en la DB
+  await producto.save();
+  console.log('POST /registro Nuevo producto exitoso estatus 201');    
+  res.status(201).json({msg:'sucess'});
+} 
+};
+        
+    
+
+
+module.exports = {
+    registroPost,
+}
