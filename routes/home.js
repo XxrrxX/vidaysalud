d07dfe8534e2b665f0c702910cd18b0839e58820
XxrrxX <79820165+XxrrxX@users.noreply.github.com>
2024-06-14 04:54:19 -0600

    Update home.js

--- routes/home.js
+++ routes/home.js
@@ -1,10 +1,13 @@
 const { Router } = require('express');
+const { vjwt } = require('../middlewares/vjwt');
+
 const {
     homeGet, 
     homePost } = require('../controllers/home');
 const router = Router();
 
 router.get('/',homeGet);
+
 router.post('/',homePost);
 
 module.exports = router;
