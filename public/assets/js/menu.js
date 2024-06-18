89f50d40fec8b8a35e2ac4e75bb38f6a6a276acf
XxrrxX <79820165+XxrrxX@users.noreply.github.com>
2024-06-18 17:52:10 -0600

    Update menu.js

--- public/assets/js/menu.js
+++ public/assets/js/menu.js
@@ -1,12 +1,16 @@
 const navToggle = document.querySelector(".nav-toggle");
 const navMenu = document.querySelector(".nav-menu");
-navToggle.addEventListener("click", () => {
-navMenu.classList.toggle("nav-menu_visible");
+const btnWha = document.querySelector(".btn-wsp");
 
+navToggle.addEventListener("click", () => {
+
+navMenu.classList.toggle("nav-menu_visible");
 if (navMenu.classList.contains("nav-menu_visible")){
 	navToggle.setAttribute("aria-label","cerrar menu");
+	btnWha.classList.toggle("btn-wsp_hide")
 	}else{
-	navToggle.setAttribute("ara-label","abrir menu");
+	navToggle.setAttribute("aria-label","abrir menu");
+	btnWha.classList.toggle("btn-wsp_hide")
 	}
 
 });
