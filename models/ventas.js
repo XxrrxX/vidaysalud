0e08c141ed8a4a5791b4ff6e633c39ba2917ab04
XxrrxX <79820165+XxrrxX@users.noreply.github.com>
2024-06-19 00:35:19 -0600

    Create ventas.js

---
+++ models/ventas.js
@@ -0,0 +1,29 @@
+const {Schema, model} = require('mongoose');
+const { type } = require('os');
+const { string } = require('yargs');
+const VentaSchema = Schema({
+    Fecha_venta:{
+        type:Date,
+        required:[true,'La fecha de venta es obligatoria'],
+    },
+    productos:{
+        type:String,
+        required:[true,'producto es obligatorio'],
+    },
+    cantidad:{
+        type:Number,
+        required:[true,'La  cantidad es obligatoria']
+    },
+    monto:{
+        type:Number,
+        required:[true,'El monto es obligatorio']
+    }
+});
+
+VentaSchema.methods.toJSON = function(){
+        const {__v,_id, ...venta} = this.toObject();
+        venta.uid = _id;
+        return venta;
+}
+
+module.exports = model('Ventas',VentaSchema);
