08ae735bfb5c99508459fafab442c0e018ccf17b
XxrrxX <79820165+XxrrxX@users.noreply.github.com>
2024-06-23 16:39:46 -0600

    Update punto_de_venta.js

--- routes/punto_de_venta.js
+++ routes/punto_de_venta.js
@@ -15,8 +15,10 @@ router.post('/',[ vjwt ],PVPOST);
 
 router.put('/',[
         check('fecha_venta','Fecha venta no valida').not().isEmpty(),
-        check('cantidad_venta','cantidad_venta no valido requiere una numero').isInt().isLength({min:1}).not().isEmpty(),
-        check('monto','Monto no valido requiere una numero').isInt().isLength({min:1}).not().isEmpty(),
+        check('productos','productos no valida').not().isEmpty(),
+        check('cantidad_venta','cantidad_venta no valido requiere un numero').isInt().isLength({min:1}).not().isEmpty(),
+        check('monto_total','Monto no valido requiere un numero').isInt().isLength({min:1}).not().isEmpty(),
+        check('usuario','usuario es requerido').not().isEmpty(),
         ValidarCampos
 ],PVPUT);
 
