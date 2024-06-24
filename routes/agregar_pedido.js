f3d339590c2f8ce217e51a73a7092a26d661729d
XxrrxX <79820165+XxrrxX@users.noreply.github.com>
2024-06-23 20:25:59 -0600

    Update agregar_pedido.js

--- routes/agregar_pedido.js
+++ routes/agregar_pedido.js
@@ -3,12 +3,16 @@ const { vjwt } = require('../middlewares/vjwt');
 
 const {
     agregar_pedidoGet, 
-    agregar_pedidoPost } = require('../controllers/agregar_pedido');
+    agregar_pedidoPost,
+    agregar_pedidoPut     } = require('../controllers/agregar_pedido');
     
 const router = Router();
 
 router.get('/',agregar_pedidoGet);
 
 router.post('/',[ vjwt ],agregar_pedidoPost);
+
+router.put('/',agregar_pedidoPut);
+
 
 module.exports = router;
